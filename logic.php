09322227c7b07cac7df86567e0031d6db3765ac9
Joachim Byrnes-Shay <jmb464@g.harvard.edu>
2016-09-22 07:06:31 -0400

    logic.php, restored broken symbol/number functionality

--- logic.php
+++ logic.php
@@ -3,20 +3,18 @@
 $contents = explode("\n", file_get_contents('data/wordsEn.txt'));#reads contents of file
 $dictionary = array_map('trim', $contents); #ensures there is no problematic whitespace in array elements, creates final wordlist
 $lastIndex = count($dictionary) - 1;  #for use in loops with random function, the highest range index used in the random func, equals index of last item in dictionary
-//$num = 5; //set default state before submit
-//$max = 5; // default state before submit because I am seeing that $max below is inside a if isset submit statement
-$symbolToAdd="";
-$numberToAdd="";
+ 
+
+
 
 $userArray = [];
 $stuff = [];
 $case = "lower";
 #the above will hold users passwords
-//$rando = -1;
+ 
 $symarray = ['!', '@', '#', '$', '%', '*', '&'];
-$errClass = "";//errEnd = "</h2>";
-//$okStart ="<h2>";
-//$okEnd = "</h2>";
+$errClass = "";
+
 	if 	(isset($_GET) && $_GET == []) {
  		$numWords = 5;
  		$max = 7;
@@ -24,6 +22,8 @@ $errClass = "";//errEnd = "</h2>";
 	else if (isset($_GET)) {
  		$numWords = $_GET["words"];
  	 	$max = $_GET["maxlength"];
+ 	 	$symbolToAdd = $symarray[rand(0,6)];
+		$numberToAdd = rand(0, 9);
  	}
 
  	 
@@ -50,70 +50,64 @@ $errClass = "";//errEnd = "</h2>";
 		else if (!(empty($_GET["case"]))) {
 			$case = $_GET["case"];
 		}
-	
- 
- 
 
-  
-echo isset($_GET)."  "; 
-var_dump($_GET);
-echo (isset($_GET["submitter"]));
  
 for ($i = 0; $i < $numWords; $i++)
 	{
-		 	$rando = rand(0, $lastIndex);
-		 while ( (strlen($dictionary[$rando]) > $max) || (in_array($dictionary[$rando], $userArray)))
+		$rando = rand(0, $lastIndex);
+		while ( (strlen($dictionary[$rando]) > $max) || (in_array($dictionary[$rando], $userArray)))
 		{
-			#for that to work I need $rando assigned to something
 			$rando = rand(0, $lastIndex);  # generate random index in value from 0 to last index value in dictionary  
 		}
 		$word = strtolower($dictionary[$rando]);
 		$wordStart =$word[0];
 		$capwordStart = strtoupper($wordStart);
-		
-			 if ($case=="upper"){
-				$word = strtoupper($word);}
-			elseif ($case == "first"){
-				$word = $capwordStart.substr($word, 1);
+		if ($case=="upper"){
+			$word = strtoupper($word);
+		}
+		elseif ($case == "first"){
+			$word = $capwordStart.substr($word, 1);
+		}
+		elseif ($case == "alternate"){
+			if (($i % 2) == 0 ) {
+				$word = strtoupper($word);
 			}
-			elseif ($case == "alternate"){
-				if (($i % 2) == 0 ) {
-					$word = strtoupper($word);
-
-			}}
-			 
+		}
 		array_push($userArray, $word);
-
-	}
+		
+		}
+	
 	 #i have my list
+   //var_dump($userArray);
+ var_dump($_GET);
 
-	#additions.
  
-			if (!(empty($_POST["symbol"])))
-
-			 {  #make sure this is the right val,ue
+ echo empty($_GET["number"]);
+ if (!(empty($userArray))){
+  if (isset($_GET["symbol"]) && $_GET["symbol"]!=""){
+	if ($_GET["symbol"] == "on") {  #make sure this is the right val,ue
 		# get last index value of last word on $user array
 		#symbol is what>
-		$symbolToAdd = $symarray[rand(0,6)];
+		
 		#make new string
-		$addSymWhere = $numWords-1;
+		$addSymWhere = 0;
 
 		$newString1 = $userArray[$addSymWhere].$symbolToAdd;
 		$userArray[$addSymWhere] = $newString1;
-	} 
-
-	if  (!(empty($_POST["number"]))) {  #make sure this is the right value
-		# get last index value of first word on $user array
-		#number is what
-		   //if ($_POST["number"] != ""){
-		$numberToAdd = rand(0, 9);
-		$addNumWhere = 0;
+	}}
+	if (isset($_GET["number"]) && $_GET["number"]!=""){
+	if (($_GET["number"] == "on")){
+		
+		$addNumWhere = $numWords-1;
 		 
+			
+		
+		 # dont try to add number to word where symbol was added
 
 		$newString1 = $userArray[$addNumWhere].$numberToAdd;
 		$userArray[$addNumWhere]=$newString1;
 		 
-	}  
-
+	}
+	} } 
 
  
