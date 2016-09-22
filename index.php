fe78c245833bef287f030a921fb581d4e0c80a14
Joachim Byrnes-Shay <jmb464@g.harvard.edu>
2016-09-22 02:01:22 -0400

    added javascript, reformatted styling, commented out php not working well

--- index.php
+++ index.php
@@ -1,61 +1,87 @@
- 
+
 <!DOCTYPE html>
 <html>
-  <head>
+  	<head>
     <title> p2 </title>
-    <link href="css\styles.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <link href="styles.css" rel="stylesheet">
     <?php require "logic.php"; ?>
-    
-  </head>
-  <body>
-  	<h1> Joachim's password generator </h1>
-  	 <div id="msgwindow">
+  	</head>
+  	<body>
+	    <h1> Joachim's password generator </h1>
+	  		<h2 id="msgwindow">
+	  	  		put stuff here
+	        	<?php if (isset($error)){echo $error;}   
+	            	elseif(isset($userArray)){ ?>
+				<!--<h2>-->
+	        	<?php foreach($userArray as $item){ ?> 
+	     	   <?php if ((isset($newString2) && $item == $newString2) || (isset($newString1) && $item == $newString1)) {echo "<span class='s'>".$item.'</span>';} else echo $item;}} ?>
+	       	</h2>
+	   	<form  method='GET' action ='index.php' id="thisform" novalidate>
+	          <!-- novalidate tag disables built-in validation messages and allows php validation to take over-->
+	    	<p class="wordconfig">  
+	       		<label for = "quant"> how many words? (3 to 9)</label>
+	      		<input id="quant" type= "number" min="3" max="9" name = "words" value="5">
+	      	</p>
+			<p class="numconfig">
+	      		<input type="checkbox"  name="number">add a number?
+			</p>
+	   		<p class="lengthconfig">
+				<label for ="max">max length (5 to 14)</label>
+	     		<input id="max" type="number" min="5" max="14" name="maxlength">
+	     	</p>
+			<p class = "symbolconfig">
+	     		<input type="checkbox"  name="symbol">add a symbol?
+	      	</p>
+	      	<p  class="caseconfig">
+	      		<input type="checkbox" id="case_config" name="config">
+	     		<label for="case_config">
+	      			configure case? 
+	      		<span>(unchecked = lowercase)</span>
+	       		</label>
+	    	</p>
+	   		<p id="switchable" class="caseoptions">
+	      		<input type ="radio" name="case" class="switch" id="upper" value="upper"><label for="upper" class="hide">all  upper-case?</label>
+	     		<input type="radio" name="case"  class="switch" id="first" value="first"><label for ="first" class="hide">cap 1st letter?</label>
+	    		<input type="radio" name="case" class="switch" id="alternate" value="alternate"> <label for="alternate" class="hide">WORD,word,WORD,word,...</label>
+	    	</p>
+	   	 	<p class = "submitbut">	
+	      		<input type="submit" name="submitter" value="submit" form="thisform"><br>
+	   		</p>
+	    </form>
+	    <script>
 
-  	  <?php if (isset($error)) {
-  	                  echo $error;} 
-  	        elseif(isset($newArray)) { ?>
-			  	   
-  	  <?php echo "<h2>";	foreach($newArray as $item) 
-  	  						{ 
-  	  	           				if (isset($symboladd) && $item==$symboladd)
-  	  	           					{echo "<span class='s'>".$item."</span>";}
-  	  	           				else 
-  	  	           				 echo $item; 
-  	  	           				}
-  	  	           			}
-  				  echo "</h2>"; ?>
-  	  
-  	 </div>
- 
- <div id="fields">
-  <form  method='POST' action ='index.php' id="thisform" novalidate>
-  <!-- novalidate tag disables built-in validation messages and allows php validation to take over-->
- 
-   <p style="margin-bottom: 0;">how many words? [3 to 9]</p><br> 
+	        var $optiondiv = document.getElementById("switchable");
+	        $optiondiv.style.visibility = "hidden";
+	        var $children = $optiondiv.childNodes;   
+	        var $labl = document.getElementsByClassName("hide");
+	        for (i=0; i< $children.length; i++) {
+	            $children[i].disabled = true;
 
-   <fieldset>
-   
-	
-  <!--<input type="number"  min=3 max=9 title="this is an error" name = "words"  ><br>-->
-  <input type="text"  pattern="[0-9]{1}"  name ="words"> <br>
-  <input type="checkbox" name="number">add a number?<br>
-  <input type="checkbox" name="symbol">add a symbol?<br>
-  <div class="maxlength">
-    <p>maxlength</p>
-  <select name="maxlength" form="thisform">
-    <option value="5">5</option>
-    <option value="10">10</option>
-    <option value="15">15</option>
-    <option value="20">20</option>
-    
-   </select>
-   
-
-  <input type="submit" name="submit" form="thisform"><br>
-  </div>
-  </fieldset>
-
-  </form>
-</div>
- </body>
-  </html>
+	        }
+	        /*for (j=0; j<$labl.length; j++) {
+	        	$labl[j].style.color = "pink";
+	        	$labl[j].style.display ="none";
+	        }*/
+	        var listen = document.getElementById("case_config");
+	        listen.onchange = function () { 
+	            var state;
+	            var col;
+	            if (listen.checked==true) {
+	                state = false;
+	                $optiondiv.style.visibility = "visible";
+	            }
+	            else {
+	                state = true;
+	            	$optiondiv.style.visibility = "hidden";
+	           	}
+	            for (i=0; i < $children.length; i++) {
+	              	$children[i].disabled = state;
+	            }
+	            /*for (j=0; j<$labl.length; j++) {
+	            	$labl[j].style.color = col;
+	            }*/
+	      	}      
+	    </script>
+	</body>
+</html>
--- logic.php
+++ logic.php
@@ -1,83 +1,118 @@
 <?php
 
+$contents = explode("\n", file_get_contents('data/wordsEn.txt'));#reads contents of file
+$dictionary = array_map('trim', $contents); #ensures there is no problematic whitespace in array elements, creates final wordlist
+$lastIndex = count($dictionary) - 1;  #for use in loops with random function, the highest range index used in the random func, equals index of last item in dictionary
+//$num = 5; //set default state before submit
+//$max = 5; // default state before submit because I am seeing that $max below is inside a if isset submit statement
+$addSymWhere = "";
+$addNumWHere = "";
 
-$testArr = ['fun', 'times', 'university', "schooldays", 'wrongway', 'gangsters', 'lotsabackers', 'despicable', 'cowardly', 'evil'];
-$newArray = []; //new array will hold password
-$blList = [];//index blacklist
-$rando=[];
-$num=5;
-$rNum = []; 
-    
+$userArray = [];
+#the above will hold users passwords
+//$rando = -1;
+$symarray = ['!', '@', '#', '$', '%', '*', '&'];
 $errStart = "<h2 class='error'>";
 $errEnd = "</h2>";
 $okStart ="<h2>";
 $okEnd = "</h2>";
 
-$symbolizer ="OFF";   
-	//validation
+
+/*if (isset($_POST["submit"])) {
+	if (!(empty($_POST["words"]))){$numWords = $_POST["words"];
+}	if (!(empty($_POST["maxlength"]))){$max = $_POST["maxlength"];}
+	if (!(empty($_POST["number"]))) {$number = $_POST["number"];}
+	if (!(empty($_POST["symbol"]))) {$symbol = $_POST["symbol"];}
+	if (!(empty($_POST["case"]))) {$case = $_POST["case"];}
+
+	
+	if (($_POST["words"]=="")) {
+		$errMsg = "you must enter # of words";
+		$error = $errStart.$errMsg.$errEnd;
+	}
+	elseif ($_POST["maxlength"]=="") {
+		$errMsg = "you must enter maximum wordlength";
+		$error = $errStart.$errMsg.$errEnd;
+	}}
+	else if (!(isset($_POST["submit"])) && (($_POST["words"]=="") || (($_POST["maxlength"]=="")))) 
+	 {$max = 8; 
+	 $numWords=5;
+	 }//, /*$_POST["case"],$_POST["maxlength"])) && $_POST["maxlength"]==""){  */
+  # used to enter 1st while loop below, or i could do, while
+# my $_POST vars are "words" for #words, "number" and "symbol checkbboxes for adding either, 
+#maxlength and minlegnth from select boxes, and submit
  
-  
- //$safeinput = htmlspecialchars($_POST["words"]);
-//echo htmlspecialchars($_POST["words"]);
-   if (isset($_POST["words"])){
-   			$safeinput = htmlspecialchars($_POST["words"]);
-   if ($safeinput =="")  {
-   			$errMsg = "you need to enter a number";
-   			$error = $errStart.$errMsg.$errEnd;
-   	  #$num = 5;
-   }
-   elseif ($safeinput < 3 || $safeinput >9) {
-   			$errMsg = "enter number between 3 and 9";
-   	 		$error = $errStart.$errMsg.$errEnd;
-   }
-	else
-   	{
-   		$num =$safeinput;//_POST["words"];
-   		//$_POST=array();
-   	}}
-   	 
-  // 	$_POST=array();
-   	for($i=0; $i<$num; $i++) {
-   		$rando=rand(0,9);
-   		while (in_array($rando, $blList)){
-   			$rando = rand(0,9);
-   		}
-   		array_push($blList, $rando);
-   	
-   			//blList is a blacklist which prevents repeat words.  this will need to be reprogrammed for scraped list
-   		if (isset($_POST["symbol"]) ) { 
-   			if ($_POST["symbol"] !== ""){
-   			$symbolizer = "ON";}
-   		} 
-   		else {
-   			($symbolizer ="OFF");  
-   		}  	
+ 
+ 
+var_dump($_POST);
+ 
+//var_dump($max);
+	#error ehcking here
+	#valldation chcking
+	# is min > max
+	# did user select word #
+	# did user select min and max
+for ($i = 0; $i < $numWords; $i++)
+	{
+		 	$rando = rand(0, $lastIndex);
+		 while ( (strlen($dictionary[$rando]) > $max) || (in_array($dictionary[$rando], $userArray)))
+		{
+			#for that to work I need $rando assigned to something
+			$rando = rand(0, $lastIndex);  # generate random index in value from 0 to last index value in dictionary  
+		}
+		$word = strtolower($dictionary[$rando]);
+		$wordStart =$word[0];
+		$capwordStart = strtoupper($wordStart);
+		
+			if (empty($_POST["case"])) {$case = "lower";}
+				elseif ($case=="upper"){
+				$word = strtoupper($word);}
+			elseif ($case == "first"){
+				$word = $capwordStart.substr($word, 1);
+			}
+			elseif ($case == "alternate"){
+				if (($i % 2) == 0 ) {
+					$word = strtoupper($word);
 
-   		if ($i< ($num - 1)) {
-   				$string = $testArr[$rando]."--"; 
-   		}//appends symbol, dashes to words except for last word
-   		else $string = $testArr[$rando]; 
+			}}
+			 
+		array_push($userArray, $word);
 
-   	 	array_push($newArray, $string);//$testArr[$rando]);*/
-   	 		//newArray is password string
-    	}
-   		echo $symbolizer;
-   		if ($symbolizer == "ON" ){ 
-   			$thing=count($newArray);
-   			//echo "thing is ".$thing;
-   			$symboladdindex = rand(0, ($thing-1));//select random word in wordlist array
-   			$symboladd = $newArray[$symboladdindex];
-   			$lenword = strlen($symboladd);//char length of that word
-   			$wordindx = rand(0, ($lenword - 3));//random character index of that word, 1 less index for -- charactr
-   			$changeit = $symboladd[$wordindx];
-   			$symboladd[$wordindx] = "*";
-   			//echo "   s-".$symboladdindex;
-   			//echo "   Wordindex is -".$wordindx;
-   			//echo "   word is ".$newArray[$symboladdindex];
-   			//echo "   characteris -".$symboladd[$wordindx];
-   			//substr_replace($symboladd, $symboladd."*", 0); 	
-   			$newArray[$symboladdindex]=$symboladd;}
-   // testing dumping webpage contents to file below
-   $myfile = fopen("new.txt", "w");
-   $tester=file("http://www.cnn.com");
-   fwrite($myfile, htmlspecialchars_decode($tester[0]));
+	}
+	 #i have my list
+
+	#additions.
+ 
+			if (!(empty($_POST["symbol"])))
+
+			 {  #make sure this is the right val,ue
+		# get last index value of last word on $user array
+		#symbol is what>
+		$symbolToAdd = $symarray[rand(0,6)];
+		#make new string
+		$addSymWhere = rand(0, ($numWords-1));
+
+		$newString1 = $userArray[$addSymWhere].$symbolToAdd;
+		$userArray[$addSymWhere] = $newString1;
+	} 
+
+	if  (!(empty($_POST["number"]))) {  #make sure this is the right value
+		# get last index value of first word on $user array
+		#number is what
+		   //if ($_POST["number"] != ""){
+		$numberToAdd = rand(0, 9);
+		$addNumWhere = rand(0, ($numWords-1));
+		while ($addNumWhere == $addSymWhere) {
+		$addNumWhere = rand(0, ($numWords-1));
+			
+		}
+		 # dont try to add number to word where symbol was added
+
+		$newString1 = $userArray[$addNumWhere].$numberToAdd;
+		$userArray[$addNumWhere]=$newString1;
+		 
+	}  
+
+//}
+//$userArray=arraytolower($userArray);
+  # this closes the if isset submit brace
